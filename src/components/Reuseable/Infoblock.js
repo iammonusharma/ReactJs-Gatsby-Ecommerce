7f9d7f7b41fa80d67e19088b12fe0868a8e99f23
iammonusharma <70936264+iammonusharma@users.noreply.github.com>
2021-07-23 16:40:31 +0530

    Update Infoblock.js

--- src/components/Reuseable/Infoblock.js
+++ src/components/Reuseable/Infoblock.js
@@ -9,10 +9,9 @@ export default function Infoblock({heading}) {
                 <div className="row">
                     <div className="col-10 col-sm-8 mx-auto text-center">
                         <p className="lead text-white mb-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Sapiente nulla esse explicabo ut minima quod hic delectus 
-                            incidunt maxime magnam.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, minima?
+                            My Name is Monu Sharma.I am a MERN stack and React Native Developer.I love programming and other tech stuff.
+                            I have a MCA degree from Guru Jambheshwar University Hisar Haryana which is a famous technological university.
+                            I love cricket and other outdoor games as well.
                         </p>
                         <Link to="/About">
                             <button className="btn btn-warning btn-lg">
